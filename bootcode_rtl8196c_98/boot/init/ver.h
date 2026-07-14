0ee8e3ef259e4704cac57b23b8272623eb090e04
kiper292 <kiper292@gmail.com>
2026-07-14 20:26:11 +0500

    Toolchain: auto-download rsdk and change build output to bin/
    - Add RSDK auto-download from GitHub releases in top-level Makefile
    - Export STAGING_DIR for Realtek wrapper binary compatibility
    - Change btcode/Makefile output paths from ../boot/Output/ to ../bin/
    - Change boot/Makefile OUTDIR and all Output references to ../bin/

--- bootcode_rtl8196c_98/boot/init/ver.h
+++ bootcode_rtl8196c_98/boot/init/ver.h
@@ -3,5 +3,5 @@
 #ifdef CONFIG_RTL89xxC
 static char B_VERSION[]="version v1.1e.1"; // 89xxC first version
 #else
-static char B_VERSION[]="version v1.1f"; //Add Dhcp and HTTP.
+static char B_VERSION[]="version v2.0"; //DAP-1360 custom bootloader
 #endif
